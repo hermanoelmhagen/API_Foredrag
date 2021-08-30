077f93aa7815e8ebff584bfaac81f128688b5651
hermanoelmhagen <89552370+hermanoelmhagen@users.noreply.github.com>
2021-08-30 16:21:20 +0200

    Update Pep8 compliance

--- trafikverket_api/api_demo.py
+++ trafikverket_api/api_demo.py
@@ -15,23 +15,24 @@ URL = "https://api.trafikinfo.trafikverket.se/v2/data.json"
 XML_FILE = "demo.xml"
 XML_FILE_STREAMING = "demo_streaming.xml"
 
-headers = {'Content-Type':'text/xml'}
+headers = {'Content-Type': 'text/xml'}
+
 
 def main(argv):
     if argv.lower() == "-static":
         static_demo(XML_FILE)
-    
+
     elif argv.lower() == "-streaming":
         streaming_demo(XML_FILE_STREAMING)
 
     else:
         print("\nSomething went wrong :(\n")
 
-        
+
 def streaming_demo(filename):
 
     xml_parsing(filename)
-    
+
     with open(filename) as xml:
         response = r.post(URL, data=xml.read(), headers=headers).json()
         SSE_URL = response["RESPONSE"]["RESULT"][0]["INFO"]["SSEURL"]
@@ -42,29 +43,30 @@ def streaming_demo(filename):
         print(msg)
         sleep(0.05)
 
-        
+
 def static_demo(filename):
-    
+
     xml_parsing(filename)
-    
+
     with open(XML_FILE) as xml:
         response = r.post(URL, data=xml.read(), headers=headers).json()
-        
+
     pprint.pprint(response)
 
-    
+
 def xml_parsing(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
     root[0].set("authenticationkey", environ["API_KEY"])
     tree.write(filename)
 
-    
+
 if __name__ == "__main__":
-    
+
     arg_list = ["-static", "-streaming"]
-    
+
     if len(sys.argv) == 2 and sys.argv[1].lower() in arg_list:
         main(sys.argv[1])
     else:
-        print(f"\nApply following argument(s): -static or -streaming\n\nExample (remove quotes): \n\t'python {sys.argv[0]} -static'\n\t'python {sys.argv[0]} -streaming'")
+        print(
+            f"\nApply following argument(s): -static or -streaming\n\nExample (remove quotes): \n\t'python {sys.argv[0]} -static'\n\t'python {sys.argv[0]} -streaming'")
